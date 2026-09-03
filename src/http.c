c8725080fa3c6f91354afd9731aeb2827b523d35
Traliran <sitetake7511@gmail.com>
2026-09-03 16:12:49 +0300

    update ver to 2.0.0-test-3

--- src/http.c
+++ src/http.c
@@ -11,7 +11,7 @@
 #include "http.h"
 
 #ifndef HUB_VERSION
-#define HUB_VERSION "1.1.0-test-2"
+#define HUB_VERSION "2.0.0-test-3"
 #endif
 
 void http_global_init(void)    { curl_global_init(CURL_GLOBAL_DEFAULT); }
--- src/util.h
+++ src/util.h
@@ -44,7 +44,7 @@ char  *format_usd(double value);
 char  *format_ts(long long ms);         /* "YYYY-MM-DD HH:MM" */
 long long now_ms(void);
 
-#define HUB_VERSION "1.1.0-test-2"
+#define HUB_VERSION "2.0.0-test-3"
 
 /* data directories: ~/.cache/traliran-cache */
 const char *hub_dir(void);
